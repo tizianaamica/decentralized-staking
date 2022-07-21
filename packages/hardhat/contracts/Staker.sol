59de6da40166790e81d5a91fe67d36ed7cb84254
tizianaamica <tizianaamicarella.tag@gmail.com>
2022-07-21 12:43:43 -0400

    modified contract and test passed

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -12,7 +12,7 @@ contract Staker {
     uint256 public constant threshold = 1 ether;
 
     //set deadline
-    uint256 public deadline = block.timestamp + 72 hours;
+    uint256 public deadline = block.timestamp + 30 seconds;
 
     bool public openForWithdraw;
 
@@ -20,11 +20,21 @@ contract Staker {
 
     event Stake(address sender, uint256 value);
 
-    event Received(address sender, uint256 value);
+    modifier deadlinePassed(bool requireDeadlinePassed) {
+        uint256 timeRemaining = timeLeft();
+        if (requireDeadlinePassed) {
+            require(timeRemaining <= 0, "Deadline has not been passed yet");
+        } else {
+            require(timeRemaining > 0, "Deadline is already passed");
+        }
+        _;
+    }
 
-    receive() external payable {
-        balances[msg.sender] += msg.value;
-        emit Received(msg.sender, msg.value);
+    /// Modifier that checks whether the external contract is completed
+    modifier stakingNotCompleted() {
+        bool completed = exampleExternalContract.completed();
+        require(!completed, "Staking period has completed");
+        _;
     }
 
     constructor(address exampleExternalContractAddress) public {
@@ -36,49 +46,31 @@ contract Staker {
     // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
     // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
 
-    function stake() public payable {
+    function stake() public payable deadlinePassed(false) stakingNotCompleted {
         balances[msg.sender] += msg.value;
         emit Stake(msg.sender, msg.value);
     }
 
     // After some `deadline` allow anyone to call an `execute()` function
-
-    /*function execute() public {
+    function execute() public stakingNotCompleted {
         // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
-        if (block.timestamp >= deadline) {
+        if (block.timestamp < deadline) {
+            revert NoSeAgotadoTiempo();
+        }
+        if (address(this).balance >= threshold) {
             exampleExternalContract.complete{value: address(this).balance}();
         } else {
             openForWithdraw = true;
         }
-    }*/
-
-     function execute() public {
-        // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
-        if (block.timestamp < deadline){
-            revert NoSeAgotadoTiempo();
-        }
-        if (address(this).balance>=threshold){
-            exampleExternalContract.complete{value: address(this).balance}() ;
-        }else {
-            openForWithdraw=true;
-        }
     }
 
     // Add a `withdraw()` function to let users withdraw their balance
-    function withdraw() public payable {
+    function withdraw() public payable deadlinePassed(true) stakingNotCompleted {
         if (openForWithdraw) {
             payable(msg.sender).transfer(address(this).balance);
         } else {
             revert();
         }
-        /*require(openForWithdraw, "Not open for withdraw");
-        //tranferir el dinero al usuario que llama la funcion
-        uint256 userBalance = balances[msg.sender];
-        require(userBalance > 0, "userBalance is 0");
-        //colocar el balance a 0
-        balances[msg.sender] = 0;
-        (bool sent, ) = _to.call{value: userBalance}("");
-        require(sent, "Failed to send to address");*/
     }
 
     // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
@@ -91,5 +83,7 @@ contract Staker {
     }
 
     // Add the `receive()` special function that receives eth and calls stake()
-
+    receive() external payable {
+        stake();
+    }
 }
